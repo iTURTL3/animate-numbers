2b2004626085813641ca83d1ac35d02f06d4ffb8
jakemadness <j4kem4dness@live.com>
2018-04-20 17:30:02 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+var numberFormat = function(number) {
+   number = String(number);
+   return number.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+var animateNumber = function(elementId) {
+   var self          = this;
+   self.element      = document.getElementById(elementId);
+   self.number       = Number(self.element.getAttribute('data-number'));
+   self.duration     = Number(self.element.getAttribute('data-duration'));
+   self.updateRate   = 1000 / 60;
+   self.totalUpdates = Math.ceil(self.duration / self.updateRate);
+   self.updates      = 0;
+   self.update       = function() {
+      self.element.innerText = numberFormat(Math.round(self.number / self.totalUpdates * self.updates));
+      if ( self.updates < self.totalUpdates ) {
+         ++self.updates;
+         setTimeout(self.update, self.updateRate);
+      }
+   };
+   self.update();
+};
